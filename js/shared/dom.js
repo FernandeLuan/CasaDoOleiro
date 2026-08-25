341fd05a9b1f54ffb3da08700c76635142da7917
Luan Fernande <110120046+FernandeLuan@users.noreply.github.com>
2026-08-25 17:15:43 -0300

    Isolate bottom navigation root from app content

--- js/shared/dom.js
+++ js/shared/dom.js
@@ -1,3 +1,9 @@
 const app=document.getElementById('app');
+let navRoot=document.getElementById('navRoot');
+if(!navRoot){
+  navRoot=document.createElement('div');
+  navRoot.id='navRoot';
+  app.insertAdjacentElement('afterend',navRoot);
+}
 const modalRoot=document.getElementById('modalRoot');
 const toastEl=document.getElementById('toast');
